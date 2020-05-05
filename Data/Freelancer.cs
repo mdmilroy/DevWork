c48a7ff14535e73e6644e582f9118bdd2df2a76f
Michael Milroy <mdmilroy@gmail.com>
2020-05-05 19:27:18 -0400

    built out employer and freelancer classes

--- Data/Freelancer.cs
+++ Data/Freelancer.cs
@@ -1,6 +1,14 @@
-﻿namespace Data
+﻿using System;
+
+namespace Data
 {
     public class Freelancer
     {
+        public Guid FreeLancerId { get; set; }
+        public string FullName { get; set; }
+        public int JobsCompleted { get; set; }
+        public double Rating { get; set; }
+        public Enum Locations { get; set; }
+        public Enum CodingLanguage { get; set; }
     }
 }
