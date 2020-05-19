96bf88ecde37f29390cde0dfc2d639a6950ae05f
Michael Milroy <mdmilroy@gmail.com>
2020-05-18 21:24:20 -0400

    removed created and modified dates

--- Models/Message/MessageListItem.cs
+++ Models/Message/MessageListItem.cs
@@ -12,6 +12,5 @@ namespace Models.Message
         public int RecipientId { get; set; }
         public string Content { get; set; }
         public bool IsRead { get; set; }
-        public DateTimeOffset ModifiedUTC { get; set; }
     }
 }
