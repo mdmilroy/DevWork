c21295b4f76b3fc8207ec625a0a98f889e40acc1
Michael Milroy <mdmilroy@gmail.com>
2020-05-30 11:47:06 -0400

    all working controllers and views to include state and user dropdowns

--- MVC/Controllers/AccountController.cs
+++ MVC/Controllers/AccountController.cs
@@ -157,7 +157,7 @@ namespace MVC.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    UserManager.AddToRole(user.Id, user.UserRole);
+                    //UserManager.AddToRole(user.Id, user.UserRole);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
